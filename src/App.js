4867ed5f44150f6d43aa938752a250414e6aafad
MeikaFreckelton <meikafreckelton@gmail.com>
2020-11-04 11:44:09 +1100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,130 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      items: props.items || [],
+      value: '',
+      
+    }
+
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+
+  }
+
+  handleChange(event){
+    this.setState({value: event.target.value})
+  }
+
+
+  handleSubmit(event){
+    this.setState({
+      // const items = state.items.concat(state.value)
+      items: [this.state.value, ...this.state.items],
+      value: ''
+      
+    })
+    
+    event.preventDefault()
+  }
+
+
+
+
+  // componentDidMount(){
+  //   const {items} = this.state
+  //   for (let i = 0; i < items.length; i++){
+  //     if (items[i].status === "completed" )
+  //   }
+  // }
+
+  // componentDidUnmount(){
+
+  // }
+
+
+  form(){
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" placeholder="to do item..." value={this.state.value} onChange={this.handleChange}/>
+          <input type="submit" value="Submit"/>
+        </form>
+      </div>
+    )
+  }
+
+  taskComplete(event){
+
+    const strikethrough = {
+      textDecoration: "line-through"
+    }
+
+    const element = event.target
+    element.classList.toggle("crossed-line")
+
+    console.log(event.target)
+
+    
+
+
+  }
+
+  taskDelete(event){
+
+
+  }
+
+
+  numberList(){
+    const {items} = this.state
+
+    
+
+    if(items){
+      const listItems = items.map((item, index) =>
+      <div>
+        <li key={index} className="tasks" onClick={this.taskComplete}>{item} </li>
+        
+        <button onClick={this.taskDelete}>✖️</button>
+      </div>
+      
+    )
+
+    return (
+      <ul>{listItems}</ul>
+    )
+    }
+    
+
+
+  }
+
+  render(){
+    const {items} = this.state
+
+    const strikethrough = {
+      textDecoration: "line-through"
+    }
+
+    // const items = [1, 2, 3, 4, 5]
+
+    
+
+    return(
+      <div>
+        <h1>To Do List</h1>
+        
+        {this.form()}
+
+        {this.numberList()}
+
+      </div>
+    )
+  }
 }
 
 export default App;
